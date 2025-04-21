8c4f50a383c3b6370b201598faf550d8d8debd40
hitogata <htogta@proton.me>
2025-04-21 15:26:58 -0400

    refactor vm test file

--- src/test_vm.c
+++ src/test_vm.c
@@ -5,20 +5,27 @@
 // defining some opcodes for debug purposes
 #define LIT2 (0b10011101)
 #define ASB2 (0b10010000)
+#define JMP2 (0b10011100)
 
-unsigned int stack_popn(OkStack* stack, unsigned char n) {
-  assert(n >= 1); assert(n <= 4); // 1-4 bytes ONLY for pop
-
-  unsigned int out = 0;
-  for (size_t i = 0; i < n; i++) {
-    out = (out << 8) | stack_pop(stack);
-  }
-  return out;
-}
+void test_multibyte();
+// void test_jmp(); TODO
+// void test_syn(); TODO
+// void test_skip(); TODO
 
 int main() {
   printf("Testing vm...\n");
   
+  test_multibyte();  
+
+  printf("Done.\n");
+  
+  return 0;
+}
+
+// TESTS BELOW:
+
+// testing asb for multi-byte ints
+void test_multibyte() {
   unsigned char program[8] = {
     LIT2, // lit2
     0x15, 
@@ -39,14 +46,14 @@ int main() {
   }
 
   // 5312 should now be on the stack top:
-  printf("Stack top: %d\n", stack_popn(&(vm.dst), 2));
+  assert(stack_popn(&(vm.dst), 2) == 5312);
+  //printf("Stack top: %d\n", stack_popn(&(vm.dst), 2));
 
   // and then 662 should be on the stack top:
-  printf("Stack top: %d\n", stack_popn(&(vm.dst), 2));
+  assert(stack_popn(&(vm.dst), 2) == 662);
+  //printf("Stack top: %d\n", stack_popn(&(vm.dst), 2));
   
   vm_free(&vm);
 
-  printf("Done.\n");
-  
-  return 0;
+  printf("...test_multibyte PASSED\n");
 }
