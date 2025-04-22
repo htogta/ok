e756d5a887863ff41957bfbd1f355cfe6f02ceed
hitogata <htogta@proton.me>
2025-04-22 00:23:32 -0400

    skip lit2 test

--- src/test_vm.c
+++ src/test_vm.c
@@ -2,17 +2,19 @@
 #include <stdio.h>
 #include <assert.h>
 
-
 void test_multibyte();
 void test_jmp();
+void test_skip();
+void test_skip_lit();
 // void test_syn(); TODO
-// void test_skip(); TODO
 
 int main() {
   printf("Testing vm...\n");
   
   test_multibyte();
   test_jmp();
+  test_skip();
+  test_skip_lit();
 
   printf("Done.\n");
   
@@ -60,6 +62,7 @@ void test_multibyte() {
   printf("...test_multibyte PASSED\n");
 }
 
+// instructions for test_jmp
 #define LIT1 (0b10001101)
 #define JMP (0b10001100)
 
@@ -84,4 +87,72 @@ void test_jmp() {
   vm_free(&vm);
   
   printf("...test_jmp PASSED\n");
+}
+
+// new instructions for test_skip
+#define ASB1_SKIP (0b11000000)
+#define MXR1 (0b10000011)
+#define DRP1 (0b10001001)
+
+void test_skip() {
+  unsigned char program[10] = {
+    LIT1,
+    0x09,
+    LIT1,
+    0x03, // stack is [9, 3] <- top
+    LIT1,
+    33, // stack is now [9, 3, 33]
+    ASB1_SKIP, // pop 33, since 33 != 255, skip ASB1
+    MXR1, // run MXR instead, stack is now [3^9, 3*9]
+    DRP1, // drop 3*9, top should be 3^9
+    0
+  };
+  
+  OkVM vm;
+  vm_init(&vm, program, 10);
+
+  vm.status = VM_RUNNING;
+  while (vm.status == VM_RUNNING) {
+    vm_tick(&vm);
+  }
+
+  assert(stack_pop(&(vm.dst)) == (3 ^ 9));
+
+  vm_free(&vm);
+
+  printf("...test_skip PASSED\n");
+}
+
+#define LIT2_SKIP (0b11011101)
+
+// gave it its own test case, because lit is a bit weird
+void test_skip_lit() {
+  // lit2_skip should skip both the "lit" instruction AND its byte args 
+  // when the top is not 255
+
+  unsigned char program[8] = {
+    LIT1, // push 69
+    69,
+    LIT1, // push 2 
+    2,
+    LIT2_SKIP, // all this gets skipped, straight to halt
+    0xcd,
+    0xab,
+    0 // should halt with 69 on the stack top
+  };
+
+  OkVM vm;
+  vm_init(&vm, program, 8);
+
+  vm.status = VM_RUNNING;
+  while (vm.status == VM_RUNNING) {
+    vm_tick(&vm);
+  }
+
+  unsigned char top = stack_pop(&(vm.dst));
+  assert(top == 69);
+
+  vm_free(&vm);
+
+  printf("...test_skip_lit PASSED\n");
 }
