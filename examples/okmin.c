fdceddf5cabb050ff218115e3ecd2d5f79244e40
hitogata <htogta@proton.me>
2025-07-18 14:46:29 -0400

    new example after major opcode rework

--- examples/okmin.c
+++ examples/okmin.c
@@ -3,7 +3,7 @@
 // okvm.h includes stdio already, so we don't need to include it again
 
 // device functions must have this signature
-unsigned char serial_output(OkVM* vm);
+unsigned char serial_output(OkVM* vm, unsigned char op);
 
 int main(int argc, char* argv[]) {
   if (argc != 2) {
@@ -12,26 +12,23 @@ int main(int argc, char* argv[]) {
   }
 
   OkVM vm;
-
-  
-  // NOTE: allocates memory for RAM and ROM
-  int init_failed = okvm_init_from_file(&vm, argv[1]);
+  int init_failed = okvm_init_from_file(&vm, argv[1]); // allocates memory
   if (init_failed) return 1;
   
-  // the binary is the device flag, least significant bit must be 1
-  int reg_failed = okvm_register_device(&vm, 0b10000000, serial_output);
+  // registering an external device 
+  int reg_failed = okvm_register_device(&vm, serial_output);
   if (reg_failed) return 1;
 
   vm.status = OKVM_RUNNING;
   while (vm.status == OKVM_RUNNING) okvm_tick(&vm);
 
-  okvm_free(&vm); // this frees the vm RAM and ROM
-
+  okvm_free(&vm); // this frees the memory allocated at init
   return vm.status != OKVM_HALTED;
 }
 
-// our device function just prints the byte at a specific address
-unsigned char serial_output(OkVM* vm) {
+// our device function just prints the byte at a specific RAM address
+unsigned char serial_output(OkVM* vm, unsigned char op) {
+  // you don't have to use the "op" argument if you don't want to
   char c = vm->ram[0xbabe];
   putchar(c);
   return c;
