5ae918bddd46498e1809133d0dc61944075d71c5
aiko <g3oneal@icloud.com>
2025-09-22 12:08:46 -0400

    fixed device registration in example

--- examples/okmin.c
+++ examples/okmin.c
@@ -1,9 +1,9 @@
-#define OKVM_IMPLEMENTATION
-#include "../okvm.h"
+#define OK_IMPLEMENTATION
+#include "../ok.h"
 // okvm.h includes stdio already, so we don't need to include it again
 
 // device functions must have this signature
-uint8_t serial_output(OkVM* vm, uint8_t op);
+uint8_t serial_output(uint8_t* ram, uint8_t* rom);
 
 int main(int argc, char* argv[]) {
   if (argc != 2) {
@@ -15,21 +15,21 @@ int main(int argc, char* argv[]) {
   int init_failed = okvm_init_from_file(&vm, argv[1]); // allocates memory
   if (init_failed) return 1;
   
-  // registering an external device 
-  int reg_failed = okvm_register_device(&vm, serial_output);
+  // registering an external device
+  // since the port is 55, the opcodes "#37 INT1" will trigger the device
+  int reg_failed = okvm_register_device(&vm, serial_output, 55);
   if (reg_failed) return 1;
 
-  vm.status = OKVM_RUNNING;
-  while (vm.status == OKVM_RUNNING) okvm_tick(&vm);
+  vm.status = OK_RUNNING;
+  while (vm.status == OK_RUNNING) okvm_tick(&vm);
 
   okvm_free(&vm); // this frees the memory allocated at init
-  return vm.status != OKVM_HALTED;
+  return vm.status != OK_HALTED;
 }
 
 // our device function just prints the byte at a specific RAM address
-uint8_t serial_output(OkVM* vm, uint8_t op) {
-  // you don't have to use the "op" argument if you don't want to
-  char c = vm->ram[0xbabe];
+uint8_t serial_output(uint8_t* ram, uint8_t* rom) {
+  char c = ram[0x37]; // here is that "magic" address
   putchar(c);
   return c;
 }
